44753fa80f8e1ac84121c270d5920c5769467cbc
Assaf Manzoor <himantz43@gmail.com>
2022-01-31 14:38:40 +0200

    Creating a CRUD using the CQRS + Mediator pattern

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,6 +1,11 @@
+using Application.Activities;
+using Application.Core;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using Persistence;
+using AutoMapper;
+using API.Extensions;
 
 namespace API
 {
@@ -18,23 +23,7 @@ namespace API
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
-
-            services.AddDbContext<DataContext>(opt =>
-            {
-                opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            });
-
-            services.AddCors(opt =>
-            {
-                opt.AddPolicy("CorsPolicy", policy =>
-                {
-                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-                });
-            });
+            services.AddApplicationExtension(_config);
 
         }
 
